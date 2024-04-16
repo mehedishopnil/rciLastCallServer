cf5319c0a304d9c3388aaa87ace28a0b9c52fa82
SK Mehedi Hasan <82324698+mehedishopnil@users.noreply.github.com>
2024-04-16 08:02:51 +0600

    server is working now

--- index.js
+++ index.js
@@ -5,41 +5,50 @@ require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-const dbUser = process.env.DB_USER;
-const dbPassword = process.env.DB_PASSWORD;
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASS);
 
-const uri = `mongodb+srv://${dbUser}:${dbPassword}@cluster0.5eoqrw2.mongodb.net/?retryWrites=true&w=majority`;
-
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5eoqrw2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 async function run() {
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+
   try {
     await client.connect();
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    const resortDataCollection = client.db("rciLastCallsDB").collection("resorts");
+
+    app.get('/resorts', async (req, res) => {
+      try {
+        const result = await resortDataCollection.find().toArray();
+        res.send(result);
+      } catch (error) {
+        console.error('Error fetching hotel data:', error);
+        res.status(500).send('Internal Server Error');
+      }
+    });
+
+    // Start the server after setting up routes and connecting to MongoDB
+    app.listen(port, () => {
+      console.log(`RCI Last Call is running on Port ${port}`);
+    });
   } catch (err) {
     console.error("Failed to connect to MongoDB:", err);
-  } finally {
-    await client.close();
   }
 }
 
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('Rci Last Call Server is running');
+  res.send('RCI Last Call server is running');
 });
 
-app.listen(port, () => {
-    console.log(`Rci Last Call Server is running on port ${port}`);
-});
